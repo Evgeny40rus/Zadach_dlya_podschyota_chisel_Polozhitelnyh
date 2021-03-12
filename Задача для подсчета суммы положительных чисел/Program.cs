3ef8fb40b12a1600b98caf0d85a02f10d6486882
Evgeny40rus <hat-99@mail.ru>
2021-03-12 19:59:12 +0300

    Добавьте файлы проекта.

---
+++ Задача для подсчета суммы положительных чисел/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Задача_для_подсчета_суммы_положительных_чисел
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            while (true)
+            {
+                Console.WriteLine("Введите число");
+                var number = Convert.ToInt32(Console.ReadLine());
+                if (number < 0)
+                {
+                    continue;
+                }
+                else if (number == 0)
+                {
+                    break;
+                }
+                //number > 0
+                sum += number;
+            }
+            Console.WriteLine("Итоговая сумма: {0}", sum);
+        }
+    }
+}
